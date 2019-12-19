b85b6e12c4c80db34f3892cf9843c71adeb1ec4b
toshi0383 <t.suzuki326@gmail.com>
2019-12-19 11:18:20 +0900

    update codebase

--- Package.swift
+++ Package.swift
@@ -1,14 +1,16 @@
-// swift-tools-version:4.0
-// Managed by ice
+// swift-tools-version:5.1
 
 import PackageDescription
 
 let package = Package(
     name: "ghaw",
+    products: [
+        .executable(name: "ghaw", targets: ["ghaw"]),
+    ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift", from: "4.2.0"),
-        .package(url: "https://github.com/JohnSundell/ShellOut", from: "2.1.0"),
-        .package(url: "https://github.com/toshi0383/CoreCLI", from: "0.1.8"),
+        .package(url: "https://github.com/toshi0383/CoreCLI", .upToNextMajor(from: "0.1.10")),
+        .package(url: "https://github.com/JohnSundell/ShellOut", .upToNextMajor(from: "2.2.0")),
+        .package(url: "https://github.com/ReactiveX/RxSwift", .upToNextMajor(from: "5.0.0")),
     ],
     targets: [
         .target(name: "ghaw", dependencies: ["RxSwift", "RxCocoa", "ShellOut", "CoreCLI"]),
